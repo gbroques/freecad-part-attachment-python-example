d6fdc7299312c49d945abe1f87b846ae1dfcc036
G Roques <groques360@gmail.com>
2020-05-07 03:10:39 -0500

    Fix name of file in link_macro.sh

--- link_macro.sh
+++ link_macro.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Convenience shell script to create a symlink of marco to ~/.FreeCAD/Macro directory
-ln -s $(pwd)/PartAttachmentExample.FCMacro ~/.FreeCAD/Macro/PartAttachmentExample.FCMacro
+ln -s $(pwd)/part_attachment_example.py ~/.FreeCAD/Macro/part_attachment_example.py
